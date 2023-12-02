63894c7da09b1a7cd300067ea9aa17bd4c69fbf0
Peter Hall <p.j.hall2008@gmail.com>
2023-12-02 17:05:04 -0500

    day 1 part 2

--- days/1/src/index.js
+++ days/1/src/index.js
@@ -1,6 +1,18 @@
 import readline from 'node:readline';
 import fs from 'node:fs';
 
+const numberMap = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+};
+
 const readStream = fs.createReadStream('../input.txt');
 
 const rl = readline.createInterface({ input: readStream });
@@ -8,13 +20,23 @@ const rl = readline.createInterface({ input: readStream });
 let total = 0;
 
 const lineParser = (line) => {
-    const regex = /[0-9]/g;
+    // Regex matching digits or number strings
+    const numbersString = Object.keys(numberMap).join('|');
+    const regex = new RegExp(`(?=([0-9]|${numbersString}))`, 'g');
 
-    // Find all numbers
-    const numbers = line.match(regex);
+    // Get a match iterator
+    const matcher = line.matchAll(regex);
+
+    const matches = [];
+    for (const y of matcher) {
+        matches.push(y[1]);
+    }
+
+    // Convert
+    const numbers = matches.map((number) => numberMap[number] ?? number);
 
     // Combine first and last numbers e.g. 1+2 = 12
-    const combined = numbers.at(0) + numbers.at(-1);
+    const combined = `${numbers.at(0)}${numbers.at(-1)}`;
 
     // Add numeric value of combined number to total
     total += Number(combined);
